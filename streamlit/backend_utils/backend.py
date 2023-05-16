91a988963695e4b544c061b80e6551b6f251988b
paulkulpin <kulpinpaul@gmail.com>
2023-05-17 00:27:39 +0400

    separating and dockerfile

---
+++ streamlit/backend_utils/backend.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI, File
+from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer
+from transformers import AutoConfig, GPT2Tokenizer
+from transformers import MBartForConditionalGeneration, MBartConfig, MBartTokenizer
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from pydantic import BaseModel
+# import os
+
+# os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+class Item(BaseModel):
+    text: str
+    min_length: int
+    max_length: int
+    length_penalty: float
+    no_repeat_ngram_size: int
+    repetition_penalty: float
+    top_k: int
+    top_p: float
+    num_beams: int
+    temperature: float
+    model_name: str
+
+app = FastAPI()
+@app.post("/summarize/")
+async def summarize(item: Item):
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+    if item.model_name == "ruT5-base":
+        config = T5Config.from_pretrained('IlyaGusev/rut5_base_sum_gazeta')
+        
+        model = T5ForConditionalGeneration(config=config)
+        model.load_state_dict(torch.load('/Users/pavelkulpin/Downloads/rut5_base_sum_gazeta_state.pth')) #, map_location=device
+        model.to(device)
+        tokenizer = T5Tokenizer.from_pretrained('IlyaGusev/rut5_base_sum_gazeta')
+    
+    elif item.model_name == "FRED-T5-Large":
+        config = AutoConfig.from_pretrained('ai-forever/FRED-T5-large')
+        
+        model = T5ForConditionalGeneration(config=config)
+        model.load_state_dict(torch.load('/Users/pavelkulpin/Downloads/fred_model_state.pth'))
+        model.to(device)
+        
+        tokenizer = GPT2Tokenizer.from_pretrained('ai-forever/FRED-T5-large', eos_token='</s>')
+    
+    elif item.model_name == "mBART":
+        config = MBartConfig.from_pretrained('IlyaGusev/mbart_ru_sum_gazeta')
+        
+        model = MBartForConditionalGeneration(config=config)
+        model.load_state_dict(torch.load('/Users/pavelkulpin/Downloads/mbart_model_state.pth')) #.to(device)
+        model.to(device)
+        tokenizer = MBartTokenizer.from_pretrained('IlyaGusev/mbart_ru_sum_gazeta')
+    
+    else:
+        raise NameError('uknown model name: {}'.format(item.model_name))
+    
+    inputs = tokenizer.encode(item.text, return_tensors="pt")
+    outputs = model.generate(inputs, 
+                             max_length=item.max_length, 
+                             min_length=item.min_length, 
+                             length_penalty=item.length_penalty, 
+                             num_beams=item.num_beams, 
+                             no_repeat_ngram_size=item.no_repeat_ngram_size, 
+                             repetition_penalty=item.repetition_penalty, 
+                             top_k=item.top_k, 
+                             top_p=item.top_p, 
+                             temperature=item.temperature)
+    
+    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return {"summary": summary}
--- streamlit/frontend_utils/frontend.py
+++ streamlit/frontend_utils/frontend.py
@@ -54,34 +54,38 @@ model_name = st.selectbox("Выберите модель генерации:", o
 sum_button = st.button("Создать аннотацию")
 
 with st.expander("Дополнительные параметры генерации"):
-    st.markdown("Рекомендация к длине аннотации. При :green[length_penalty>0] будут поощряться более длинные тексты, при length_penalty < 0 - более короткие.")
+    st.markdown("Рекомендация к длине аннотации. При :green[length_penalty>0] будут поощряться более длинные тексты, при :green[length_penalty<0] - более короткие.")
     length_penalty = st.number_input("length_penalty", min_value=-10.0, max_value=20.0, value=1.0, step=0.1)
     st.divider()
-
-    st.markdown("Ограничения генерируемого текста по числу токенов. В среднем слово кодируется 1-3 токенами.")
-    min_length = st.slider("min_length:", min_value=10, max_value=100, value=40, step=1)
-    max_length = st.slider("max_length:", min_value=min_length, max_value=500, value=150, step=1)
-    st.divider()
-
-    st.markdown("Контроль повторений. Чем выше значение, тем реже будут встречаться повторения. При :green[repetition_penalty=1] штрафа за повторения нет.")
-    repetition_penalty = st.number_input("repetition_penalty:", min_value=1.0, max_value=10.0, value=1.0, step=0.1)
-    st.divider()
     
-    st.markdown("Строгое ограничений повторений. При :green[no_repeat_ngram_size>0] последовательности символов соответсвующей длины не будут иметь возможности повторения.")
-    no_repeat_ngram_size = st.number_input("no_repeat_ngram_size:", min_value=0, max_value=10, value=3, step=1)
-    st.divider()
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.markdown("Контроль повторений. Чем выше значение, тем реже будут встречаться повторения. При :green[repetition_penalty=1] штрафа за повторения нет.")
+        repetition_penalty = st.number_input("repetition_penalty:", min_value=1.0, max_value=10.0, value=1.0, step=0.1)
+        st.divider()
     
-    st.markdown("Ограничение числа наиболее вероятных кандидатов при генерации следующего токена. :green[top_k] указывает на число кандидатов, а :green[top_p] отсекает долю наименее вероятные варианты по статистическую вероятность.")
-    top_k = st.number_input("top_k:", min_value=0, max_value=100, value=50, step=1)
-    top_p = st.number_input("top_p:", min_value=0.0, max_value=1.0, value=1.0, step=0.1)
-    st.divider()
+        st.markdown("Строгое ограничений повторений. При :green[no_repeat_ngram_size>0] последовательности символов соответсвующей длины не будут иметь возможности повторения.")
+        no_repeat_ngram_size = st.number_input("no_repeat_ngram_size:", min_value=0, max_value=10, value=3, step=1)
+        st.divider()
 
-    st.markdown('Количество "сценариев", рассматриваемых моделью при аннотировании, для определения лучшей последовательности слов. Увеличение :green[num_beams] может улучшить результат, но замедлит процесс. При :green[num_beams=1] генерация будет жадной.')
-    num_beams = st.slider("num_beams:", min_value=1, max_value=10, value=4, step=1)
-    st.divider()
+        st.markdown("Ограничения генерируемого текста по числу токенов. В среднем слово кодируется 1-3 токенами.")
+        min_length = st.slider("min_length:", min_value=10, max_value=100, value=40, step=1)
+        max_length = st.slider("max_length:", min_value=min_length, max_value=500, value=150, step=1)
+        st.divider()
 
-    st.markdown("Контроль разнообразия генерации. Чем выше :green[temperature], тем стабильнее, но детерминированнее результат.")
-    temperature = st.number_input("temperature:", min_value=0.1, max_value=10.0, value=0.7, step=0.1)
+    with col2:
+        st.markdown("Ограничение числа наиболее вероятных кандидатов при генерации следующего токена. :green[top_k] указывает на число кандидатов, а :green[top_p] отсекает долю наименее вероятные варианты по статистическую вероятность.")
+        top_k = st.number_input("top_k:", min_value=0, max_value=100, value=50, step=1)
+        top_p = st.number_input("top_p:", min_value=0.0, max_value=1.0, value=1.0, step=0.1)
+        st.divider()
+
+        st.markdown('Количество "сценариев", рассматриваемых моделью при аннотировании, для определения лучшей последовательности слов. Увеличение :green[num_beams] может улучшить результат, но замедлит процесс. При :green[num_beams=1] генерация будет жадной.')
+        num_beams = st.slider("num_beams:", min_value=1, max_value=10, value=4, step=1)
+        st.divider()
+    
+        st.markdown("Контроль разнообразия генерации. Чем ниже :green[temperature], тем детерменированнее и надежнее будет результат. Чем выше, тем нестабильнее, но многообразнее будет генерация.")
+        temperature = st.number_input("temperature:", min_value=0.1, max_value=10.0, value=0.7, step=0.1)
 
 
 
@@ -95,13 +99,11 @@ if 'recent_params' not in st.session_state:
     st.session_state['recent_params'] = []
 
 if sum_button:
-    # summary = summarize(text, min_length, max_length, length_penalty, 
-    #                     no_repeat_ngram_size, repetition_penalty, top_k, top_p,
-    #                     num_beams, temperature, model_name)
-    summary = 'example text.'
+    summary = summarize(text, min_length, max_length, length_penalty, 
+                        no_repeat_ngram_size, repetition_penalty, top_k, top_p,
+                        num_beams, temperature, model_name)
+    # summary = 'example text.'
     st.session_state['summary'] = summary
-
-    st.session_state['recent_summaries'].insert(0, summary)
     tmp_params = {
         "min_length": min_length, 
         "max_length": max_length, 
@@ -114,18 +116,20 @@ if sum_button:
         "temperature": temperature, 
         "model_name": model_name
     }
+
+    st.session_state['recent_summaries'].insert(0, summary)
     st.session_state['recent_params'].insert(0, tmp_params)
 
-    if len(st.session_state['recent_summaries']) > 5:
-        st.session_state['recent_summaries'] = st.session_state['recent_summaries'][:5]
-        st.session_state['recent_params'] = st.session_state['recent_params'][:5]
+    if len(st.session_state['recent_summaries']) > 10:
+        st.session_state['recent_summaries'] = st.session_state['recent_summaries'][:10]
+        st.session_state['recent_params'] = st.session_state['recent_params'][:10]
 
 
 
 header2 = st.header("Аннотация:")
 if st.session_state['summary'] != '':
     st.divider()
-    st.text(st.session_state['summary'])
+    st.write(st.session_state['summary'])
     st.divider()
 
 copy_sum_button = st.button("Скопировать результат")
@@ -144,6 +148,7 @@ with st.expander("Сохранить"):
     else:
         st.error('Сначала сгенерируйте аннотацию.', icon="❌")
 
+# if st.session_state['recent_summaries']:
 header3 = st.header("Последние генерации:")
 for recent_sum, recent_params in zip(st.session_state['recent_summaries'], st.session_state['recent_params']):
     st.write(recent_sum)
